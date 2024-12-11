cba4b2bd56e6c688d7331f76e89f554afe475f33
Ömer Aydin <omer.aydin2@student.rocva.nl>
2024-12-11 09:41:51 +0100

    opdracht views

--- example-app/resources/views/planets.blade.php
+++ example-app/resources/views/planets.blade.php
@@ -3,11 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Planets</title>
 </head>
 <body>
-    @foreach ($planets as $e)
-    <li>{{ $e }}</li>
-    @endforeach
+    <ul>
+        @foreach ($planets as $planet)
+            <li>
+                <h2>{{ $planet['name'] }}</h2>
+                <p>{{ $planet['description'] }}</p>
+            </li>
+        @endforeach
+    </ul>
 </body>
 </html>
