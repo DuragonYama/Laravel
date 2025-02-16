86f2a668e62fdf7d98c7b91737896a7f5179b898
Ömer Aydin <omer.aydin2@student.rocva.nl>
2025-02-16 16:07:42 +0100

    clockwork

--- example-app/resources/views/index.blade.php
+++ example-app/resources/views/index.blade.php
@@ -4,15 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Planets</title>
-</head>
+    </head>
 <body>
     <ul>
         @foreach ($planets as $planet)
             <li>
-                <h2><a href="{{ route('planets.show', $planet['name']) }}">{{ $planet['name'] }}</a></h2>
-                <p>{{ $planet['description'] }}</p>
+                <h2><a href="{{ route('planets.show', $planet->name) }}">{{ $planet->name }}</a></h2>
+                <p>{{ $planet->description }}</p>
             </li>
         @endforeach
     </ul>
 </body>
 </html>
+
